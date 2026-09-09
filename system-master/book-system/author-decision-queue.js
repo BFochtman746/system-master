6d389b32f746221702000a9a5cb21210b6cfdb78
BFochtman746 <85698619+BFochtman746@users.noreply.github.com>
2026-09-09 19:31:39 -0400

    books: harden public author decision queue surface

--- system-master/book-system/author-decision-queue.js
+++ system-master/book-system/author-decision-queue.js
@@ -1,6 +1,44 @@
 'use strict';
 
 const queue = require('./author-decision-queue-core.js');
+const vr = require('./version-and-rollback.js');
+
+function validateParentQueue(parentState, queueLedger) {
+  vr.validateParentState(parentState);
+  queue.validateQueueLedger(queueLedger, parentState);
+}
+
+function enqueueDecision(args) {
+  validateParentQueue(args.parentState, args.queueLedger);
+  return queue.enqueueDecision(args);
+}
+function presentDecision(args) {
+  validateParentQueue(args.parentState, args.queueLedger);
+  return queue.presentDecision(args);
+}
+function deferDecision(args) {
+  validateParentQueue(args.parentState, args.queueLedger);
+  return queue.deferDecision(args);
+}
+function reopenDecision(args) {
+  validateParentQueue(args.parentState, args.queueLedger);
+  return queue.reopenDecision(args);
+}
+function withdrawDecision(args) {
+  validateParentQueue(args.parentState, args.queueLedger);
+  return queue.withdrawDecision(args);
+}
+function resolveDecision(args) {
+  validateParentQueue(args.parentState, args.queueLedger);
+  vr.validateLedger(args.versionLedger, args.parentState);
+  return queue.resolveDecision(args);
+}
+function revalidateAgainstParent(args) {
+  vr.validateParentState(args.previousParentState);
+  vr.validateParentState(args.currentParentState);
+  queue.validateQueueLedger(args.queueLedger, args.previousParentState);
+  return queue.revalidateAgainstParent(args);
+}
 
 module.exports = {
   AuthorDecisionQueueError: queue.AuthorDecisionQueueError,
@@ -13,12 +51,12 @@ module.exports = {
   digestDecisionRequest: queue.digestDecisionRequest,
   validateQueueLedger: queue.validateQueueLedger,
   createQueueLedger: queue.createQueueLedger,
-  enqueueDecision: queue.enqueueDecision,
-  presentDecision: queue.presentDecision,
-  deferDecision: queue.deferDecision,
-  reopenDecision: queue.reopenDecision,
-  withdrawDecision: queue.withdrawDecision,
+  enqueueDecision,
+  presentDecision,
+  deferDecision,
+  reopenDecision,
+  withdrawDecision,
   presentationProjection: queue.presentationProjection,
-  resolveDecision: queue.resolveDecision,
-  revalidateAgainstParent: queue.revalidateAgainstParent,
+  resolveDecision,
+  revalidateAgainstParent,
 };
