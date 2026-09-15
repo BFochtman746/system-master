30132ba0e6808a6ed17e815e413c4e5d44bd6ad4
BFochtman746 <85698619+BFochtman746@users.noreply.github.com>
2026-09-15 18:28:44 -0400

    Book B05-E: isolate nested adversarial test runners

--- system-master/book-system/book-b05-e-adversarial.test.js
+++ system-master/book-system/book-b05-e-adversarial.test.js
@@ -28,11 +28,17 @@ function rejectObservationField(key, expectedCode, value = 'forbidden') {
   }), expectedCode);
 }
 
+function isolatedChildEnv() {
+  const env = { ...process.env };
+  delete env.NODE_TEST_CONTEXT;
+  return env;
+}
+
 function runExisting(file, pattern) {
   const result = spawnSync(process.execPath, ['--test', `--test-name-pattern=${pattern}`, file], {
     cwd: ROOT,
     encoding: 'utf8',
-    env: process.env
+    env: isolatedChildEnv()
   });
   const output = `${result.stdout || ''}\n${result.stderr || ''}`;
   assert.equal(result.status, 0, output);
